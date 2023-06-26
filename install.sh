d6ec1717d5910290474819f3b023b79b3f5d2165
tom <yinchunkai@hotmail.com>
2023-06-26 15:55:30 -0400

    add install

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 # Install packages
 yum -y update
 yum install -y nano tree python3
-sudo dnf install python3-pip
+sudo dnf install -y python3-pip
 yum install -y git
 sudo yum install -y postgresql
 dnf install java-11-openjdk
@@ -14,10 +14,14 @@ pip3 install --user psycopg2
 
 # config/install custom software
 cd /home/ec2-user
-git clone https://github.com/cdavidshaffer/python-image-gallery.git
+git clone https://github.com/yinchunkai/python-image-gallery.git
 chown -R ec2-user:ec2-user python-image-gallery
+sudo chmod 755 -R python-image-gallery
+
+pip3 install -r python-image-gallery/requirements.txt --user
+./python-image-gallery/debug
 
 # start/enable services
-systemctl stop postfix
-systemctl disable postfix
-~                              
+#systemctl stop postfix
+#systemctl disable postfix
+
